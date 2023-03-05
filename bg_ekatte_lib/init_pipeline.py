b3c7d3f1b603affc27712f793197af45422420f9
Todor Lubenov <tlubenov@gmail.com>
2023-03-05 13:57:42 +0200

    update pipelines

--- bg_ekatte_lib/init_pipeline.py
+++ bg_ekatte_lib/init_pipeline.py
@@ -1,5 +1,5 @@
 import os
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Any
 from pathlib import Path
 from datetime import datetime
 
@@ -18,6 +18,15 @@ def plot_map(gdf: gpd, to_file: str):
     plt.savefig(to_file)
 
 
+def drop_fields(df, columns: List) -> Any:
+    for col in columns:
+        try:
+            del df[col]
+        except Exception as ex:
+            print(ex)
+    return df
+
+
 def get_ekatte_df(base_path: str) -> List:
 
     df = pd.read_json(os.path.join(base_path, 'ek_atte.json'))
@@ -28,9 +37,7 @@ def get_ekatte_df(base_path: str) -> List:
     release_date = list(df[df[release_field].notna()][release_field].values)[0]
     generation_date = list(df[df[generation_field].notna()][generation_field].values)[0]
 
-    del df[generation_field]
-    del df[release_field]
-
+    df = drop_fields(df, [generation_field, release_field])
     df = df.drop_duplicates()
     df = df.dropna(how='all')
     df.to_parquet('../data/processed/v20230131/ekatte.pq', index=False)
@@ -38,7 +45,6 @@ def get_ekatte_df(base_path: str) -> List:
 
 
 def get_ekatte_bbox(base_shape_path: str) -> List:
-
     gdf = gpd.read_file(base_shape_path)
     gdf.rename(columns={'EKATTE_1': 'ekatte'}, inplace=True)
 
@@ -50,14 +56,9 @@ def get_ekatte_bbox(base_shape_path: str) -> List:
     gdf_wgs84 = pd.concat([gdf_wgs84, gdf_wgs84.bounds], axis=1)
     gdf_wgs84['ext_geometry'] = gdf_wgs84.apply(lambda row: get_bbox_polygon(row), axis=1)
     gdf_wgs84.rename(columns={'geometry': 'srs_geometry', 'ext_geometry': 'geometry'}, inplace=True)
-
-    del gdf_wgs84["srs_geometry"]
-    del gdf_wgs84["minx"]
-    del gdf_wgs84["miny"]
-    del gdf_wgs84["maxx"]
-    del gdf_wgs84["maxy"]
-
+    gdf_wgs84 = drop_fields(df=gdf_wgs84, columns=["srs_geometry", "minx", "miny", "maxx", "maxy"])
     gdf_wgs84.set_crs("EPSG:4326", inplace=True)
+
     gdf_wgs84.to_feather("../data/processed/v20230131/ekatte_geo.feather")
 
     gdf_3857 = gdf_wgs84.to_crs("EPSG:3857")
@@ -71,11 +72,8 @@ if __name__ == "__main__":
     Path('/mapsapps/projects/bg-ekatte-lib/data/processed').mkdir(parents=True, exist_ok=True)
     Path('/mapsapps/projects/bg-ekatte-lib/data/processed/v20230131/').mkdir(parents=True, exist_ok=True)
 
-
     BASE_PATH = "/mapsapps/projects/bg-ekatte-lib/data/source_data/v_202212_202301/Ekatte_json/"
     df_ekatte = get_ekatte_df(base_path=BASE_PATH)
 
     BG_ZEMS = '/mapsdata/sources/ekatte_shapefile/ekatte.shp'
     df_ekatte_geom = get_ekatte_bbox(base_shape_path=BG_ZEMS)
-
-
